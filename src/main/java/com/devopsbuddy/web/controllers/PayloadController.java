3bf411790a958769123b86406f5af543e347f4b7
joseph mhanna <joseph.mhanna@teltacworldwide.com>
2018-03-16 11:00:58 +0200

    create payload page

---
+++ src/main/java/com/devopsbuddy/web/controllers/PayloadController.java
@@ -0,0 +1,18 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by j_mha on 3/16/2018.
+ */
+@Controller
+public class PayloadController {
+
+    public static final String PAYLOAD_VIEW_NAME="payload/payload";
+
+    @RequestMapping("/payload")
+    public String payload(){
+        return PAYLOAD_VIEW_NAME;
+    }
+}
